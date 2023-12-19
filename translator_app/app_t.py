2039c805548da9c908a9843ad2d523606a14f564
Nurislam Akbulatov <122386960+Mdabo1@users.noreply.github.com>
2023-12-19 19:07:12 +0500

    Update app_t.py

--- translator_app/app_t.py
+++ translator_app/app_t.py
@@ -5,16 +5,8 @@ from languages import *
 st.title("Переводчик текста")
 source_text = st.text_area("Введите текст для перевода:")
 target_language = st.selectbox("Выберите целевой язык:", languages)
-translate = st.button('Перевести')
+translate = st.button("Перевести")
 if translate:
     translator = Translator()
     out = translator.translate(source_text, dest=target_language)
     st.write(out.text)
-
-
-
-
-
-
-
-
